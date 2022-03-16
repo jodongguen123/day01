e463c95f13708d8019bab7af301f0a2c7c926ae4
JoDongGeun <jokun2006@naver.com>
2022-03-16 14:08:14 +0900

    계산

---
+++ src/day01/Ex02.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		System.out.println("==================================");
+		System.out.println("이름" + "\t  " + "나이" + "\t" + "전화번호" + "\t    " + "회비");
+		System.out.println("==================================");
+		System.out.println("홍길동" + "\t" + " \" " + 15 + "\" " + 3672 + "-" + 1234 + "    \\200000");
+		System.out.println("고길동" + "\t" + " \" " + 15 + "\" " + 2238 + "-" + 1234 + "    \\300000");
+		System.out.println("김말이" + "\t" + " \" " + 15 + "\" " + 1234 + "-" + 1234 + "    \\500000");
+		System.out.println("----------------------------------");
+		System.out.println("총합계" + "\t\t\t" + "    \\100000");
+		System.out.println("----------------------------------");
+	}
+}
